316469d54052c96f8793175c200c64a1bdf69cb4
Vladislav Kutsevich <kutsevichvlad18@gmail.com>
2023-06-03 00:09:11 +0300

    update
    add header
    add function to submit form

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,21 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+// import {} from './App.styled';
+import { Searchbar } from '../Searchbar/Searchbar';
+// import { ToastContainer } from 'react-toastify';
+export class App extends Component {
+  state = {
+    query: '',
+  };
+
+  handleSubmit = query => {
+    this.setState({ query });
+  };
+
+  render() {
+    return (
+      <div>
+        <Searchbar onSubmit={this.handleSubmit} />
+      </div>
+    );
+  }
+}
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,55 @@
+import { Component } from 'react';
+import {
+  Header,
+  SearchForm,
+  SearchFormButton,
+  SearchFormButtonLabel,
+  SearchFormInput,
+} from './Searchbar.styled';
+import Notiflix from 'notiflix';
+// import { toast } from 'react-toastify';
+
+export class Searchbar extends Component {
+  state = {
+    query: '',
+  };
+
+  changeInput = event => {
+    this.setState({ query: event.currentTarget.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const trimQuery = this.state.query.trim();
+
+    if (trimQuery === '') {
+      Notiflix.Notify.failure('Поле ввода пустое');
+      return;
+    }
+    Notiflix.Notify.success(`вот что мы нашли по запросу ${this.state.query}`);
+    this.props.onSubmit(trimQuery);
+    this.setState({ query: '' });
+  };
+
+  render() {
+    return (
+      <Header>
+        <SearchForm onSubmit={this.handleSubmit}>
+          <SearchFormButton type="submit">
+            <SearchFormButtonLabel>Search</SearchFormButtonLabel>
+          </SearchFormButton>
+
+          <SearchFormInput
+            name="query"
+            value={this.state.query}
+            type="text"
+            autocomplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            onChange={this.changeInput}
+          />
+        </SearchForm>
+      </Header>
+    );
+  }
+}
