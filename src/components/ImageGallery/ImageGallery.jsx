7a48c10ae1f5f6d874f711f77e5ef78255411dfd
Vladislav Kutsevich <kutsevichvlad18@gmail.com>
2023-06-04 11:47:01 +0300

    update

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,7 +1,9 @@
 import { Component } from 'react';
+import { getImages } from '../servise/api';
 import { GalleryList, TextContainer, Text } from './ImageGallery.styled';
 import { ImageGalleryItem } from '../ImageGalleryItem/ImageGalleryItem';
 import { Loader } from '../Loader/Loader';
+import { LoadMore } from '../Button/Button';
 
 const URL = 'https://pixabay.com/api/';
 const API_KEY = '35615782-928d74ab541d750ac5cbbfeab';
@@ -15,11 +17,44 @@ export class ImageGallery extends Component {
     perPage: 12,
   };
 
-  componentDidUpdate(prevProps, prevState) {
+  async componentDidUpdate(prevProps, prevState) {
     const prevProp = prevProps.query;
     const nextProp = this.props.query;
+    const nextState = this.state;
+
+    console.log(prevState.page);
+    console.log(this.state.page);
+    console.log(prevState.images);
+    console.log(this.state.images);
 
     if (prevProp !== nextProp) {
+      this.setState({ status: 'pending', page: 1 });
+
+      try {
+        const array = await getImages(this.props.query, this.state.page);
+        this.setState({ images: array.hits, status: 'resolved' });
+      } catch (error) {
+        this.setState({ error: error.message, status: 'rejected' });
+      }
+
+      // fetch(
+      //   `${URL}?q=${this.props.query}&page=${this.state.page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${this.state.perPage}`
+      // )
+      //   .then(response => {
+      //     if (response.ok) {
+      //       return response.json();
+      //     }
+      //     return Promise.reject(
+      //       new Error(`Нет фото по  запросу ${this.props.query}`)
+      //     );
+      //   })
+      //   .then(images =>
+      //     this.setState({ images: images.hits, status: 'resolved' })
+      //   )
+      //   .catch(error => this.state({ error, status: 'rejected' }));
+    }
+
+    if (prevState.page !== nextState.page) {
       this.setState({ status: 'pending' });
 
       fetch(
@@ -33,10 +68,19 @@ export class ImageGallery extends Component {
             new Error(`Нет фото по  запросу ${this.props.query}`)
           );
         })
-        .then(images => this.setState({ images, status: 'resolved' }))
+        .then(images =>
+          this.setState(prevState => ({
+            images: [...prevState.images, ...images.hits],
+            status: 'resolved',
+          }))
+        )
         .catch(error => this.state({ error, status: 'rejected' }));
     }
   }
+
+  handlerLoadMore = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
 
   getImageId = imgId => {
     console.log(imgId);
@@ -60,7 +104,7 @@ export class ImageGallery extends Component {
     if (status === 'rejected') {
       return (
         <TextContainer>
-          <Text>{error.message}</Text>
+          <Text>{error}</Text>
         </TextContainer>
       );
     }
@@ -74,12 +118,15 @@ export class ImageGallery extends Component {
 
     if (status === 'resolved' && images.total !== 0) {
       return (
-        <GalleryList>
-          <ImageGalleryItem
-            images={this.state.images}
-            onClick={this.getImageId}
-          />
-        </GalleryList>
+        <>
+          <GalleryList>
+            <ImageGalleryItem
+              images={this.state.images}
+              onClick={this.getImageId}
+            />
+          </GalleryList>
+          <LoadMore onClick={this.handlerLoadMore} />
+        </>
       );
     }
   }
--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -3,7 +3,7 @@ import { GalleryItem, GalleryImg } from './ImageGalleryItem.styled';
 export const ImageGalleryItem = ({ images, onClick }) => {
   return (
     <>
-      {images.hits.map(img => (
+      {images.map(img => (
         <GalleryItem key={img.id} onClick={() => onClick(img.id)}>
           <GalleryImg src={img.webformatURL} alt={img.tags} />
         </GalleryItem>
